376545d9b1d12e6975dc3802fd9f207053aeb4fe
SnickerDev <64985447+sohan9819@users.noreply.github.com>
2023-08-07 23:41:19 +0530

    fixed authenticated routes

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,10 @@
 import Navbar from "./Navbar";
 import Header from "./Header";
 // import Footer from "./Footer";
-import React from "react";
+import React, { useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { useRouter } from "next/router";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,13 +12,19 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   const { data: sessionData } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!sessionData?.user) {
+      void router.replace("/");
+    }
+  }, [sessionData]);
 
   return (
-    // <div className="h-max min-h-screen bg-[#191919] px-4">
     <div className="h-max min-h-screen bg-[#0d1117] px-4">
       <Header />
       <Navbar />
-      {sessionData ? <main>{children}</main> : <main>{children}</main>}
+      <main>{children}</main>
       <Toaster position="top-right" />
     </div>
   );
